1b6a8c1e7e8ac0bda363bce273f42e1aa9b4884c
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-08-16 17:11:38 -0300

    Initial commit

---
+++ CSSPTaskRunnerCSV.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerCSV.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerCSV.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2636a7d3-2cc7-44bc-b56f-9c89489689ad")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CSSPTaskRunnerCSVHelper.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CSSPTaskRunnerCSVHelper.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Environment Canada")]
+[assembly: AssemblyProduct("CSSPTaskRunnerCSVHelper.Tests")]
+[assembly: AssemblyCopyright("Copyright © Environment Canada 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1fd7be82-8d9d-4214-93c9-95f60561770b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
